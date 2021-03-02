c72b6dc584b97528d5c3c3b77f172247fead275b
leeks123 <kensei719@naver.com>
2021-03-02 14:30:31 +0900

    21/03/02

---
+++ BOJ/Classify/Dynamic/15489_파스칼삼각형.py
@@ -0,0 +1,19 @@
+r, c, w = map(int, input().split())
+
+p = [[0]*(r+w) for _ in range(r+w)]
+
+for i in range(r+w):
+    for j in range(r+w):
+        if j == 0:
+            p[i][j] = 1
+        elif j == i:
+            p[i][j] = 1
+        else:
+            p[i][j] = p[i-1][j-1] + p[i-1][j]
+
+answer = 0
+for i in range(r-1, r-1+w):
+    for j in range(c-1, c+1+i-r):
+        print(p[i][j], i, j)
+        answer += p[i][j]
+print(answer)
