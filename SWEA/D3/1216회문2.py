da3b8e69015546440efb53568b1b22aae899c577
leeks123 <kensei719@naver.com>
2021-04-15 13:33:26 +0900

    210415

--- SWEA/D3/1216회문2.py
+++ SWEA/D3/1216회문2.py
@@ -5,19 +5,22 @@ def isPalindrome(arr):
     return True
 
 
-def countPalindrome(board, n):
-    cnt = 0
+def countPalindrome(board):
+    ret = 0
     for arr in board:
-        for i in range(8 - n + 1):
-            if isPalindrome(arr[i:i+n]):
-                cnt += 1
+        for n in range(100, 1, -1):
+            for i in range(100 - n + 1):
+                if isPalindrome(arr[i:i+n]):
+                    if ret < n:
+                        ret = n
+    return ret
 
 
 def rotateBoard(board):
     ret = []
-    for i in range(8):
+    for i in range(100):
         arr = []
-        for j in range(8):
+        for j in range(100):
             arr.append(board[j][i])
         ret.append(arr)
     return ret
@@ -26,12 +29,12 @@ def rotateBoard(board):
 def solve():
     n = int(input())
     board = []
-    for _ in range(8):
+    for _ in range(100):
         board.append(list(input()))
-    answer = countPalindrome(board, n)
+    answer = countPalindrome(board)
     board = rotateBoard(board)
-    answer += countPalindrome(board, n)
-    return answer
+    answer2 = countPalindrome(board)
+    return max(answer, answer2)
 
 
 for i in range(1, 11):
